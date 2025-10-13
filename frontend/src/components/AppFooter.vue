7ec68acc631b7edc9f6b53214a9c74f37fa8bd0e
Churk_Ben <2779675416@qq.com>
2025-10-13 23:59:34 +0800

    feat: 更新home页

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,21 +1,21 @@
 <template>
-	<n-card :bordered="false">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<span class="mb-0">&copy; 2025 JuFire Studio. All rights reserved.</span>
-				</div>
-				<div class="col-md-6 text-end">
-					<n-button text tag="a" href="https://github.com" class="me-3" target="_blank">
-						<i class="fab fa-github me-2"></i>GitHub
-					</n-button>
-					<n-button text tag="a" href="/contact" class="me-3">
-						<i class="fas fa-envelope me-2"></i>Contact
-					</n-button>
-				</div>
-			</div>
-		</div>
-	</n-card>
+  <n-card :bordered="false">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <span class="mb-0">&copy; 2025 JuFire Studio. All rights reserved.</span>
+        </div>
+        <div class="col-md-6 text-end">
+          <n-button text tag="a" href="https://github.com" class="me-3" target="_blank">
+            <i class="fab fa-github me-2"></i>GitHub
+          </n-button>
+          <n-button text tag="a" href="/contact" class="me-3">
+            <i class="fas fa-envelope me-2"></i>Contact
+          </n-button>
+        </div>
+      </div>
+    </div>
+  </n-card>
 </template>
 
 <script setup lang="ts">
@@ -24,7 +24,7 @@ import { NCard, NButton } from 'naive-ui';
 
 <style scoped>
 .n-card {
-	font-size: medium;
-	font-weight: 300;
+  font-size: medium;
+  font-weight: 300;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -35,8 +35,28 @@
       </template>
     </n-card>
   </n-space>
+
+  <n-space>
+    <n-avatar round size="small" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+    <n-avatar round size="medium" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+    <n-avatar round size="large" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+    <n-avatar round :size="48" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+    <n-avatar :style="{
+      color: 'yellow',
+      backgroundColor: 'red',
+    }">
+      M
+    </n-avatar>
+  </n-space>
+
 </template>
 
 <script setup lang="ts">
-import { NButton, NSpace, NCard } from 'naive-ui'
+import { NButton, NSpace, NCard, NAvatar } from 'naive-ui'
 </script>
+
+<style scoped>
+.n-space {
+  margin-bottom: 20px;
+}
+</style>
