6ea6e9c6145831e7a89d7bb8412416a3d6aef570
churk_ben <2779675416@qq.com>
2025-11-02 15:29:24 +0800

    feat: 优化登录表单布局，添加占位符并移除多余组件

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,25 +1,13 @@
 <template>
-    <n-card embedded title="登录" class="col-4 mx-auto my-auto">
+    <n-card embedded title="登录" class="col-3 mx-auto my-auto">
         <!-- 登录表单 -->
         <n-form ref="formRef" :model="modelRef" :rules="rules">
             <n-form-item path="username" label="用户名">
-                <n-input v-model:value="modelRef.username" @keydown.enter.prevent />
+                <n-input v-model:value="modelRef.username" placeholder="请输入用户名" @keydown.enter.prevent />
             </n-form-item>
             <n-form-item path="password" label="密码">
-                <n-input v-model:value="modelRef.password" type="password" @input="handlePasswordInput"
-                    @keydown.enter.prevent />
+                <n-input v-model:value="modelRef.password" type="password" placeholder="请输入密码" @keydown.enter.prevent />
             </n-form-item>
-
-            <n-row :gutter="[0, 24]">
-                <n-col :span="24">
-                    <div style="display: flex; justify-content: flex-end">
-                        <n-button :disabled="modelRef.username === null" round type="primary"
-                            @click="handleValidateButtonClick">
-                            验证
-                        </n-button>
-                    </div>
-                </n-col>
-            </n-row>
         </n-form>
 
         <!-- 底部链接 -->
@@ -32,29 +20,20 @@
 
         <template #action>
             <n-button type="primary" block size="large" @click="handleLogin">
+                <i class="fas fa-sign-in-alt me-2"></i>
                 登录
             </n-button>
         </template>
 
     </n-card>
-
-
-    <pre>{{ JSON.stringify(modelRef, null, 2) }}</pre>
 </template>
 
 <script setup lang="ts">
-import { NButton, NSpace, NCard, NAvatar, NTag, NDropdown, NFlex } from 'naive-ui'
-import { useMessage } from 'naive-ui'
-import { ref } from 'vue'
-
-function handleLogin() {
-    // 处理登录逻辑
-    console.log("登录按钮被点击");
-}
-
-import { NForm, NFormItem, NInput, NRow, NCol } from 'naive-ui'
 import type { FormInst, FormItemInst, FormItemRule, FormRules } from 'naive-ui'
-
+import { NButton, NCard, NForm, NFormItem, NInput } from 'naive-ui'
+import { useMessage } from 'naive-ui'
+import { stringify } from 'querystring'
+import { ref } from 'vue'
 
 interface ModelType {
     username: string | null
@@ -62,71 +41,33 @@ interface ModelType {
 }
 
 const formRef = ref<FormInst | null>(null)
-const rPasswordFormItemRef = ref<FormItemInst | null>(null)
 const message = useMessage()
+
 const modelRef = ref<ModelType>({
     username: null,
     password: null
 })
 
-function validatePasswordStartWith(rule: FormItemRule, value: string): boolean {
-    return (
-        !!modelRef.value.password
-        && modelRef.value.password.startsWith(value)
-        && modelRef.value.password.length >= value.length
-    )
-}
-
-function validatePasswordSame(rule: FormItemRule, value: string): boolean {
-    return value === modelRef.value.password
-}
-
 const rules: FormRules = {
-    age: [
+    username: [
         {
             required: true,
-            validator(rule: FormItemRule, value: string) {
-                if (!value) {
-                    return new Error('需要年龄')
-                }
-                else if (!/^\d*$/.test(value)) {
-                    return new Error('年龄应该为整数')
-                }
-                else if (Number(value) < 18) {
-                    return new Error('年龄应该超过十八岁')
-                }
-                return true
-            },
-            trigger: ['input', 'blur']
+            message: '至少输个用户名啊喂!'
         }
     ],
     password: [
         {
             required: true,
-            message: '请输入密码'
-        }
-    ],
-    reenteredPassword: [
-        {
-            required: true,
-            message: '请再次输入密码',
-            trigger: ['input', 'blur']
-        },
-        {
-            validator: validatePasswordStartWith,
-            message: '两次密码输入不一致',
-            trigger: 'input'
-        },
-        {
-            validator: validatePasswordSame,
-            message: '两次密码输入不一致',
-            trigger: ['blur', 'password-input']
+            message: '还需要密码'
         }
     ]
 }
 
-function handlePasswordInput() {
-    rPasswordFormItemRef.value?.validate('password-input')
+function handleLogin() {
+    // 验证密码格式
+    handleValidateButtonClick(new MouseEvent('click'))
+    // 处理登录逻辑
+    console.log("登录按钮被点击" + JSON.stringify(modelRef.value, null, 2));
 }
 
 function handleValidateButtonClick(e: MouseEvent) {
